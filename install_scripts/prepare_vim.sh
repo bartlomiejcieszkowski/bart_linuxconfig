12851c9833dd1393eb66149bf5bd267747778b1a
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-04-12 07:11:34 +0200

    switch to Vundle

---
+++ install_scripts/prepare_vim.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WORKING_DIR=`pwd`
+SCRIPT_DIR=`basename "$0"`
+
+type curl &> /dev/null
+if [ $? -ne 0 ]; then
+	sudo apt-get install -y curl
+fi
+
+type python &> /dev/null
+if [ $? -ne 0 ]; then
+	sudo apt-get install -y python
+fi
+
+cd ${SCRIPT_DIR}
+
+#pathogen apt-vim
+#./install_apt-vim.sh
+
+#source ~/.bashrc || source ~/.bash_profile
+
+#apt-vim install -y https://github.com/scrooloose/nerdtree.git
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "set nocompatible" >> ~/.vimrc
+echo "filetype off" >> ~/.vimrc
+echo "set rtp+=~/.vim/bundle/Vundle.vim" >> ~/.vimrc
+echo "call vundle#begin()" >> ~/.vimrc
+echo "\" PLUGINS_BEGIN" >> ~/.vimrc
+
+echo "Plugin 'https://github.com/scrooloose/nerdtree.git'" >> ~/.vimrc
+echo "\" PLUGINS_END" >> ~/.vimrc
+echo "call vundle#end()" >> ~/.vimrc
+echo "filetype plugin indent on" >> ~/.vimrc
+#echo "filetype plugin on" >> ~/.vimrc
+
+vim +PluginInstall +qall
+
