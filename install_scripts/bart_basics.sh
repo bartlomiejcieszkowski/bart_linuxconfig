a7151117c3b22f195d16eccdb4cac8a0c3946df9
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2020-04-17 09:01:28 +0200

    organize script, dont run apt-get always but only when needed
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

--- install_scripts/bart_basics.sh
+++ install_scripts/bart_basics.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-set -x
+#set -x
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install tmux
-sudo apt-get install vim
+STEP_NAME="[vim]"
+if command -v vim >/dev/null 2>&1 ; then
+	echo "${STEP_NAME}[OK] vim is installed"
+else
+	echo "${STEP_NAME}[FAIL] vim is not installed"
+	sudo apt-get install vim || exit 1
+fi
 
-echo "Customizing vim"
+echo "${STEP_NAME}Customizing vim"
 if [ ! -d ~/.vim/colors ]; then
 	mkdir -p ~/.vim/colors
 fi
 
 if [ ! -f ~/.vim/colors/monokai.vim ]; then
-	echo "Getting monokai theme"
+	echo "${STEP_NAME}Getting monokai theme"
 	wget -O ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim
 fi
 
 if [ ! -f ~/.vim/colors/material-monokai.vim ]; then
-	echo "Getting material-monokai theme"
+	echo "${STEP_NAME}Getting material-monokai theme"
 	wget -O ~/.vim/colors/material-monokai.vim https://raw.githubusercontent.com/skielbasa/vim-material-monokai/master/colors/material-monokai.vim
 fi
 
 touch ~/.vimrc
 if ! grep -F "syntax" ~/.vimrc; then
+	echo "${STEP_NAME}Enabling syntax highlighting"
 	echo "syntax enable" >> ~/.vimrc
 fi
 
@@ -39,10 +43,20 @@ fi
 #fi
 
 if ! grep -Fx "colorscheme material-monokai" ~/.vimrc; then
+	echo "${STEP_NAME}Setting colorscheme to material-monokai"
 	echo "colorscheme material-monokai" >> ~/.vimrc
 fi
 
+STEP_NAME="[tmux]"
+if command -v tmux >/dev/null 2>&1 ; then
+	echo "${STEP_NAME}[OK] tmux is installed"
+else
+	echo "${STEP_NAME}[FAIL] tmux is not installed"
+	sudo apt-get install tmux || exit 1
+fi
+
 if [ ! -f ~/.tmux.conf ]; then
+	echo "${STEP_NAME}settings some sane defaults in tmux"
 	echo 'set -g default-terminal "tmux-256color"' > ~/.tmux.conf
 	echo 'bind r source-file ~/.tmux.conf' >> ~/.tmux.conf
 	echo '# switch panes using Alt-arrow without prefix' >> ~/.tmux.conf
@@ -52,4 +66,6 @@ if [ ! -f ~/.tmux.conf ]; then
 	echo 'bind -n M-Down select-pane -D' >> ~/.tmux.conf
 	echo '# Enable mouse mode (tmux 2.1 and above)' >> ~/.tmux.conf
 	echo '#set -g mouse on' >> ~/.tmux.conf
+else
+	echo "${STEP_NAME}tmux.conf already exists, skipping"
 fi
